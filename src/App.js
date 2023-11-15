a934133b077e5d834cba274a52dba75c5b1aa8bf
skhswe99 <skhswe99@gmail.com>
2023-11-14 21:48:10 -0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+  
+    return (
+      <div>
+        <h1>{course}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {part1} {exercises1}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </div>
+    )
+  }
 
-export default App;
+  export default App;
